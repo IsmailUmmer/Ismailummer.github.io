92ca619b35fabab2adfbb5bad2812368d7814c5c
IsmailUmmer <ktismail92@gmail.com>
2022-10-04 21:10:22 +0530

    themetoggler fix

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-let nav = document.querySelector('#navbarbtn');
-let navcont = document.querySelector('.nav__cont');
+let menu = document.querySelector('#navbarbtn');
+let header = document.querySelector('.nav__cont');
 
-nav.onclick = () =>{
-    nav.classList.toggle('fa-times');
-    navcont.classList.toggle('active');
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
 }
 
-let themeToggler = document.querySelector('#theme-toggler');
+function toggleClasses(btn, b1, b2){
+    btn.classList.toggle(b1);
+    btn.classList.toggle(b2);
+}
+function mytoggleClasses(btn, b1, b2) {
+    toggleClasses(themeToggler, "fa-sun", "fa-moon");
+  }
 
+let themeToggler = document.querySelector('#theme-toggler');
 themeToggler.onclick = () =>{
-    themeToggler.classList.toggle('fa-sun');
+    themeToggler.classList.toggle(mytoggleClasses()); 
     if(themeToggler.classList.contains('fa-sun')){
         document.body.classList.add('active');
     }else{
         document.body.classList.remove('active');
+        
     }
 }
