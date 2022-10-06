8d60d7217f761b8f1d62242cf4ec49b9153a54f3
IsmailUmmer <ktismail92@gmail.com>
2022-10-06 16:53:36 +0530

    quick fix

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 let menu = document.querySelector('#navbarbtn');
 let header = document.querySelector('.nav__cont');
 
-menu.onclick = () =>{
+menu.onclick = () => {
     menu.classList.toggle('fa-times');
     header.classList.toggle('active');
 }
 
-function toggleClasses(btn, b1, b2){
+let themeToggler = document.querySelector('#theme-toggler');
+function toggleClasses(btn, b1, b2) {
     btn.classList.toggle(b1);
     btn.classList.toggle(b2);
 }
 function mytoggleClasses(btn, b1, b2) {
     toggleClasses(themeToggler, "fa-sun", "fa-moon");
-  }
+}
 
-let themeToggler = document.querySelector('#theme-toggler');
-themeToggler.onclick = () =>{
-    themeToggler.classList.toggle(mytoggleClasses()); 
-    if(themeToggler.classList.contains('fa-sun')){
+themeToggler.onclick = () => {
+    themeToggler.classList.toggle(mytoggleClasses());
+    if (themeToggler.classList.contains('fa-sun')) {
         document.body.classList.add('active');
-    }else{
+    } else {
         document.body.classList.remove('active');
-        
     }
 }
